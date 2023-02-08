985890d8beee704a58e7edbaa82a48ee694e4bca
Rodolfo Andre <racaballerov@outlook.com>
2023-02-08 14:22:30 -0500

    feat: Añadiendo listado y detalle de mascota

--- app/blueprints/pet/__init__.py
+++ app/blueprints/pet/__init__.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request 
 from app.models.breed import Breed
 from app.models.pet import Pet
 from app.forms.pet_form import PetForm
-from flask import request
+from sqlalchemy import asc
 from app import db
 
 bp = Blueprint('pet', __name__, template_folder='templates')
 
 @bp.route('/')
 def index():
-  return render_template('index.html')  
+  pets = Pet.query.order_by(Pet.birth_date.desc(), Pet.name.desc(), Pet.owner_name.desc()).all()
+
+  return render_template('index.html', pets=pets)  
 
 @bp.route('/pet', methods=['GET', 'POST'])
 def add_pet():
@@ -28,3 +30,13 @@ def add_pet():
     return render_template('index.html')
 
   return render_template('add_pet.html', form=pet_form)
+
+@bp.route('/pet/<int:id>', methods=['GET'])
+def detail_pet(id):
+  pet_object = Pet.query.get_or_404(id)
+
+  return render_template("detail_pet.html", pet=pet_object)
+
+
+
+
